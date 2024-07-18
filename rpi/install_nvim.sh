edcb2246b1d08b53d9638039fa85511dd9c8ec49
Harry Conner <harrymconner@gmail.com>
2024-07-17 20:33:43 -0400

    fix typo in neovim github url

--- rpi/install_nvim.sh
+++ rpi/install_nvim.sh
@@ -5,7 +5,7 @@ sudo apt update && sudo apt upgrade
 # these are the dependencies for nvim and kickstart
 sudo apt install ninja-build gettext cmake unzip curl build-essential make gcc ripgrep xclip
 
-git clone https:/dgithub.com/neovim/neovim && cd neovim && git checkout stable
+git clone https://github.com/neovim/neovim && cd neovim && git checkout stable
 make CMAKE_BUILD_TYPE=Release
 cd build
 cpack -G DEB && sudo dpkg -i nvim-linux64.deb
