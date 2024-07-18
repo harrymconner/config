86f3bcd210e09057aa184b8e97b8347adeaf25a8
Harry Conner <harrymconner@gmail.com>
2024-07-17 20:52:42 -0400

    general enhancements

--- rpi/install_nvim.sh
+++ rpi/install_nvim.sh
@@ -2,17 +2,20 @@
 
 cd $HOME
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade && /
 
 # these are the dependencies for nvim and kickstart
 sudo apt install ninja-build gettext cmake unzip curl build-essential make gcc ripgrep xclip
 
-git clone https://github.com/neovim/neovim && cd neovim && git checkout stable
-make CMAKE_BUILD_TYPE=Release
-cd build
+git clone https://github.com/neovim/neovim && cd neovim && git checkout stable && /
+make CMAKE_BUILD_TYPE=Release && /
+cd build && /
 cpack -G DEB && sudo dpkg -i nvim-linux64.deb
-cd ../..
+
+cd $HOME && /
 sudo rm -rf neovim
 
 git clone https://github.com/harrymconner/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+cd .config/nvim && /
+git pull
 
