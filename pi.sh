c24c01dcb31b2b21a890d4682f91da3cb8e87cc2
Harry Conner <harrymconner@gmail.com>
2024-12-28 13:13:43 -0500

    add docker

--- pi.sh
+++ pi.sh
@@ -10,6 +10,9 @@ curl -LsSf https://astral.sh/uv/install.sh | sh &&
 source $HOME/.local/bin/env &&
 uv python install
 
+# --- docker ---
+sudo apt-get install docker.io
+
 # --- neovim ---
 # cd $HOME
 
