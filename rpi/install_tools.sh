f4c93d7ab14ef92c4de51c709370bd462e385c0b
Harry Conner <harrymconner@gmail.com>
2024-12-28 12:52:29 -0500

    put all tools in one script

--- rpi/install_tools.sh
+++ rpi/install_tools.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install git
+
+# --- git ---
+sudo apt-get install git &&
+curl "https://raw.githubusercontent.com/harrymconner/config/refs/heads/main/git/.gitconfig" > .gitconfig
+
+# --- uv ---
+curl -LsSf https://astral.sh/uv/install.sh | sh &&
+source $HOME/.local/bin/env &&
+uv python install
+
+# --- neovim ---
+# cd $HOME
+
+# sudo apt-get update && sudo apt-get upgrade &&
+
+# # these are the dependencies for nvim and kickstart
+# sudo apt-get install ninja-build gettext cmake unzip curl build-essential make gcc ripgrep xclip
+
+# git clone https://github.com/neovim/neovim && cd neovim && git checkout stable && /
+# make CMAKE_BUILD_TYPE=Release && /
+# cd build && /
+# cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+
+# cd $HOME && /
+# sudo rm -rf neovim
+
+# git clone https://github.com/harrymconner/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+# cd .config/nvim && /
+# git pull
