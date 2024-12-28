68821d47df77c8b8b5f431184c9deeea1eeaf6c6
Harry Conner <harrymconner@gmail.com>
2024-12-28 12:31:13 -0500

    start sketching out tools script

--- rpi/install_git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt update && sudo apt upgrade
-sudo apt install git
-git config --global user.name "Harry Conner"
-git config --global user.email "harrymconner@gmail.com"
-git config --global push.autosetupremote true
-git config --global init.defaultBranch main
